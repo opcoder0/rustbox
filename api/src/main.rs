ac52771e8f8cbf2ff5cf8a3f4f0ef08bc0ab9207
opcoder0 <110003254+opcoder0@users.noreply.github.com>
2022-10-21 15:30:11 +1100

    fix api by specifying lifetime and sharing references

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,8 +1,8 @@
 trait FooTrait {
     type ValueType;
-    fn geta(&self) -> String;
+    fn geta(&self) -> &String;
     fn getb(&self) -> i32;
-    fn getc(&self) -> Option<Self::ValueType>;
+    fn getc(&self) -> Option<&Self::ValueType>;
 }
 
 #[derive(Debug)]
@@ -15,20 +15,20 @@ struct Foo<T> {
 impl<T> FooTrait for Foo<T> {
     type ValueType = T;
 
-    fn geta(&self) -> String {
-        self.a
+    fn geta(&self) -> &String {
+        &self.a
     }
 
     fn getb(&self) -> i32 {
         self.b
     }
 
-    fn getc(&self) -> Option<T> {
-        self.c
+    fn getc(&self) -> Option<&T> {
+        self.c.as_ref()
     }
 }
 
-impl<T> Foo<T> {
+impl<T: 'static> Foo<T> {
     fn new(a: String, b: i32, c: Option<T>) -> Box<dyn FooTrait<ValueType = T>> {
         Box::new(Self { a, b, c })
     }
